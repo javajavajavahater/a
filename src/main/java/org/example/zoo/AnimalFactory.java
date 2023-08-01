870aa447e18de177cac2e9ca2aa325e15da606b8
myName <blala@gmail.com>
2023-08-01 16:27:24 +0300

    fix(AnimalException)- change class name, change(animalsNames.txt)- change the text fix(IncorrectFileNameException.java, FileMethods.java, Animal.Factory.java)- fix code standards

--- src/main/java/org/example/zoo/AnimalFactory.java
+++ src/main/java/org/example/zoo/AnimalFactory.java
@@ -1,20 +1,20 @@
 package org.example.zoo;
 
-import org.example.exeptions.AnimalExeption;
+import org.example.Main;
+import org.example.exeptions.AnimalException;
 import org.example.exeptions.NotFoundAnimalNameException;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class AnimalFactory {
-    private static final String ANIMALS_CLASSES_PACKAGE = "org.example.zoo.";
-
-    public Animal createAnimal(String animalName) throws NotFoundAnimalNameException, AnimalExeption {
+    public Animal createAnimal(String animalName) throws AnimalException {
         try {
-            return (Animal) Class.forName(ANIMALS_CLASSES_PACKAGE + animalName).getDeclaredConstructor().newInstance();
-        } catch (InvocationTargetException | InstantiationException |  IllegalAccessException |NoSuchMethodException e) {
-            throw new AnimalExeption("something went wrong", e);
-        } catch (ClassNotFoundException e) {
-            throw new NotFoundAnimalNameException("the class: " + animalName + " not exist", e);
+            return (Animal) Class.forName(Main.ANIMALS_CLASSES_PACKAGE + animalName).getDeclaredConstructor().newInstance();
+        } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new AnimalException("something went wrong", e);
+        } catch (ClassNotFoundException | NullPointerException e) {
+            throw new NotFoundAnimalNameException(String.format("the animal: %s not exist", animalName), e);
         }
     }
 }
--- src/main/java/org/example/zoo/FileMethods.java
+++ src/main/java/org/example/zoo/FileMethods.java
@@ -2,16 +2,20 @@ package org.example.zoo;
 
 import org.example.exeptions.IncorrectFileNameException;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileMethods {
+
+    private FileMethods() {
+    }
 
     public static String returnLineTextFromFile(String fileName) throws IncorrectFileNameException {
         try (Scanner readerFile = new Scanner(new File(fileName))) {
             return readerFile.nextLine();
         } catch (FileNotFoundException e) {
-            throw new IncorrectFileNameException("incorrect file name, check if the argument is the correct name of the file", e);
+            throw new IncorrectFileNameException(String.format("incorrect file name %s,check if the argument is the correct name of the file", fileName), e);
         }
     }
 }
