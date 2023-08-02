deeb3f12fe1241c1e11973c207d8c8baa31eea4c
myName <blala@gmail.com>
2023-08-02 15:43:38 +0300

    fix(PropertiesFileMethods.java) change "returnProperties" method

--- src/main/java/org/example/zoo/PropertiesFileMethods.java
+++ src/main/java/org/example/zoo/PropertiesFileMethods.java
@@ -5,23 +5,21 @@ import org.example.exeptions.PropertiesException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFileMethods {
-    private PropertiesFileMethods() {}
-
-    public static Properties returnProperties(InputStream propertiesFileInputStream) throws IncorrectFileNameException, PropertiesException {
-        try {
-            Properties properties = new Properties();
-            properties.load(propertiesFileInputStream);
-            return properties;
-        } catch (FileNotFoundException e) {
-            throw new IncorrectFileNameException(String.format("incorrect file name %s", propertiesFileInputStream), e);
-        } catch (IOException e) {
-            throw new PropertiesException(String.format("failed read %s file", propertiesFileInputStream), e);
-        }
+    private PropertiesFileMethods() {
     }
 
-
+    public static Properties returnProperties(String propertiesFileName) throws IncorrectFileNameException, PropertiesException {
+        try {
+            Properties properties = new Properties();
+            properties.load(FileMethods.getResourceFileAsInputStream(propertiesFileName));
+            return properties;
+        } catch (FileNotFoundException e) {
+            throw new IncorrectFileNameException(String.format("incorrect file name %s", propertiesFileName), e);
+        } catch (IOException e) {
+            throw new PropertiesException(String.format("failed read %s file", propertiesFileName), e);
+        }
+    }
 }
