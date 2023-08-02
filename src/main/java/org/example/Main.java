9906507481e5ada227c2709f0a6578d71c5c70c8
myName <blala@gmail.com>
2023-08-02 14:37:07 +0300

    Fix(Main.java): changed because AnimalFactory constructor changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ import org.example.exeptions.PropertiesException;
 import org.example.zoo.Animal;
 import org.example.zoo.AnimalFactory;
 import org.example.zoo.FileMethods;
+import org.example.zoo.PropertiesFileMethods;
 
 public class Main {
     private static final int ANIMALS_NAMES_FILE_PATH_INDEX = 0;
     private static final String DELIMITER_ANIMAL_NAME_FROM_TEXT = " ";
-    public static final String ANIMALS_CLASSES_PACKAGE = "org.example.zoo.";
     private static final String ANIMAL_PROPERTIES_FILE_NAME = "animal.properties";
 
     public static void main(String[] args) throws IncorrectFileNameException, AnimalException, PropertiesException {
@@ -21,7 +21,7 @@ public class Main {
 
     private static void createAnimalAndImplementsAnimalClassMethods(String[] animalsNames)
             throws AnimalException, IncorrectFileNameException, PropertiesException {
-        AnimalFactory animalFactory = new AnimalFactory(ANIMAL_PROPERTIES_FILE_NAME);
+        AnimalFactory animalFactory = new AnimalFactory(PropertiesFileMethods.returnProperties(ANIMAL_PROPERTIES_FILE_NAME));
 
         for (String animalName : animalsNames) {
             Animal animal = animalFactory.createAnimal(animalName);
