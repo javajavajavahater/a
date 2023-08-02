72c7fdc0857994c71c107dfbea96bd9bf4abc8b1
myName <blala@gmail.com>
2023-08-02 10:47:33 +0300

    fix(AnimalExeption.java, animalsNames.txt): fix AnimalExeption class name, change text in animalNames.txt

--- src/main/java/org/example/exeptions/AnimalExeption.java
+++
@@ -1,7 +0,0 @@
-package org.example.exeptions;
-
-public class AnimalExeption extends Exception{
-    public AnimalExeption(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
