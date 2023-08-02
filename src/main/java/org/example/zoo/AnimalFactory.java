da63da0d5e4d5461415e44bdbd817728756c8d0c
myName <blala@gmail.com>
2023-08-02 11:40:01 +0300

    fix(AnimalFactory):  add Constructor,changed "checkAnimalNameInAnimalProperties" method name to "getAnimalClassName", and moved from main class to animalFactory class.

--- src/main/java/org/example/zoo/AnimalFactory.java
+++ src/main/java/org/example/zoo/AnimalFactory.java
@@ -2,14 +2,27 @@ package org.example.zoo;
 
 import org.example.Main;
 import org.example.exeptions.AnimalException;
+import org.example.exeptions.IncorrectFileNameException;
 import org.example.exeptions.NotFoundAnimalNameException;
+import org.example.exeptions.PropertiesException;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Properties;
 
 public class AnimalFactory {
+    private Properties animalProperties;
+
+    public AnimalFactory(String propertiesFileName) throws IncorrectFileNameException, PropertiesException {
+        this.animalProperties = PropertiesFileMethods.returnProperties(FileMethods.getResourceFileAsInputStream(propertiesFileName));
+    }
+
+    private static String getAnimalClassName(Properties animalProperties, String animalName) {
+        return Main.ANIMALS_CLASSES_PACKAGE + animalProperties.getProperty(animalName, animalName);
+    }
+
     public Animal createAnimal(String animalName) throws AnimalException {
         try {
-            return (Animal) Class.forName(Main.ANIMALS_CLASSES_PACKAGE + animalName).getDeclaredConstructor().newInstance();
+            return (Animal) Class.forName(getAnimalClassName(this.animalProperties, animalName)).getDeclaredConstructor().newInstance();
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException |
                  NoSuchMethodException e) {
             throw new AnimalException("something went wrong", e);
